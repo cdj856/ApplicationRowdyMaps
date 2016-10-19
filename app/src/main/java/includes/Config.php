e67faffca91d6bf48463c8198871073194441f2a
jonathan <jayembee21@hotmail.com>
2016-10-19 01:48:53 -0500

    Here is the php were gunna use, might need to be reformatted. Still need to understand the loginactivity before i do anything with it

---
+++ app/src/main/java/includes/Config.php
@@ -0,0 +1,11 @@
+Config.php
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "bifrost_madmap");
+define("DB_PASSWORD", "tPP3M2cBQsMUsVjq");
+define("DB_DATABASE", "bifrost_madmap");
+?>
