c1b4a07e0a95ee4d7a34bc427eb7c5b127b8b537
jonathan <jayembee21@hotmail.com>
2016-10-25 23:33:49 -0500

    .............?????????????????????????????????????????

--- app/src/main/java/com/rowdy/marvinlopez/applicationrowdymaps/LoginActivity.java
+++ app/src/main/java/com/rowdy/marvinlopez/applicationrowdymaps/LoginActivity.java
@@ -3,6 +3,7 @@ package com.rowdy.marvinlopez.applicationrowdymaps;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -87,6 +88,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        RegisterActivity.class);
+
+                startActivity(i);
             }
         });
 
