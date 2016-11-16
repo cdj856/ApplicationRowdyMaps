166f3cffd9b32c34e7023ec58beb1884fe20983c
marvinlopez <cdj856@my.utsa.edu>
2016-11-16 11:57:36 -0600

    Updated Code November--ML added functions and buttons for friends feature.

---
+++ app/src/main/java/com/rowdy/marvinlopez/applicationrowdymaps/FriendsActivity.java
@@ -0,0 +1,10 @@
+package com.rowdy.marvinlopez.applicationrowdymaps;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by marvinlopez on 11/16/16.
+ */
+
+public class FriendsActivity extends AppCompatActivity {
+}
