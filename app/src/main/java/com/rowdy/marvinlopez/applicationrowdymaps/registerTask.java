a25b7641af1913a5538894691d1055973f03698c
jonathan <jayembee21@hotmail.com>
2016-11-16 07:31:31 -0600

    same error different code...may be an issue with the output writer

--- app/src/main/java/com/rowdy/marvinlopez/applicationrowdymaps/registerTask.java
+++ app/src/main/java/com/rowdy/marvinlopez/applicationrowdymaps/registerTask.java
@@ -9,6 +9,12 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.lang.*;
+import java.security.*;
+import java.io.StringWriter;
+
+import org.apache.http.client.*;
+
+
 
 /**
  * Created by jonathan on 11/14/2016.
@@ -18,6 +24,7 @@ public class registerTask extends AsyncTask<String, String, String> {
         protected String doInBackground(String...insert) {
             Log.d("task err", "" + insert[0]);
             try {
+                //Setting connection and method
                 URL url = new URL("https://easel1.fulgentcorp.com/bifrost/ws.php?json=");
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
                 con.setDoOutput(true);
@@ -27,22 +34,55 @@ public class registerTask extends AsyncTask<String, String, String> {
                 con.setRequestProperty("Accept", "application/json");
                 con.setRequestMethod("POST");
 
-                JSONArray arr = new JSONArray();
-                JSONObject act = new JSONObject();
-                act.put("action", "run_sql");
-                JSONObject query = new JSONObject();
-                query.put("query", insert[0]);
-                JSONObject sess = new JSONObject();
-                sess.put("session_id", new Integer(1));
-                JSONObject checksum = new JSONObject();
-                checksum.put("checksum","1");
-                arr.put(act);
-                arr.put(query);
-                arr.put(sess);
-                arr.put(checksum);
-                Log.d("Task err2", "" + arr.toString());
+                JSONObject json = new JSONObject();
+                json.put("someKey", "someValue");
+
+                //Creating JSON array of JSON objects
+                JSONArray arrLogin = new JSONArray();
+                JSONObject actLog = new JSONObject();
+                actLog.put("action", "login");
+                JSONObject login = new JSONObject();
+                login.put("login", "fa2016_madmappers");
+                JSONObject pass = new JSONObject();
+                pass.put("password", "28e501ff6289a7b00015bd19cf92b3342d7eba1af47864374d3dbc1b9e4ec919");
+                JSONObject app = new JSONObject();
+                app.put("app_code", "fpcwvmCthCnAbvs5");
+                JSONObject sessType = new JSONObject();
+                sessType.put("session_type", "id_salt");
+
+                //Add these to JSON array before checksum (they are required to get checksum)
+                arrLogin.put(actLog);
+                arrLogin.put(login);
+                arrLogin.put(pass);
+                arrLogin.put(app);
+                arrLogin.put(sessType);
+
+                //Hash JSON array to get checksum
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                md.update(arrLogin.toString().getBytes());
+
+                byte byteData[] = md.digest();
+
+                StringBuffer sbuf = new StringBuffer();
+                for (int i = 0; i < byteData.length; i++) {
+                    sbuf.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+                }
+
+                String newChecksum = sbuf.toString();
+
+                //Create JSON object for the calculated checksum and append that to the array
+                JSONObject checksumLogin = new JSONObject();
+                checksumLogin.put("checksum", newChecksum);
+                arrLogin.put(checksumLogin);
+
+                String test = "[{\"action\":\"login\"},{\"login\":\"fa2016_madmappers\"},{\"password\":\"28e501ff6289a7b00015bd19cf92b3342d7eba1af47864374d3dbc1b9e4ec919\"},{\"app_code\":\"fpcwvmCthCnAbvs5\"},{\"session_type\":\"id_salt\"},{\"checksum\":\"844c323475c7244b8734cb3f28a7cd6f33b64fcc15b330c98db257705c5ec62a\"}]";
+
+                String thing = JSONObject.quote(arrLogin.toString());
+                String thing2 = thing.substring(1, thing.length()-1);
+                Log.d("Task err2", test);
+
                 OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
-                wr.write(arr.toString());
+                wr.write(test);
                 wr.flush();
 
                 StringBuilder sb = new StringBuilder();
@@ -55,11 +95,29 @@ public class registerTask extends AsyncTask<String, String, String> {
                         sb.append(line + "\n");
                     }
                     br.close();
-                    Log.d("success err", "" + sb.toString());
+                    Log.d("response err", "" + sb.toString());
                     return sb.toString();
                 } else {
                     Log.d("conn err", con.getResponseMessage());
                 }
+
+
+
+
+                /* Keep for run_sql when i get this shit finally working
+                JSONArray arr = new JSONArray();
+                JSONObject act = new JSONObject();
+                act.put("action", "run_sql");
+                JSONObject query = new JSONObject();
+                query.put("query", insert[0]);
+                JSONObject sess = new JSONObject();
+                sess.put("session_id", new Integer(1));
+                JSONObject checksum = new JSONObject();
+                checksum.put("checksum","1");
+                arr.put(act);
+                arr.put(query);
+                arr.put(sess);
+                arr.put(checksum);*/
 
 
             }catch(Exception e){
